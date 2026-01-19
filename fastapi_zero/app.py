7d4f38627d1f572ae532d211bce46a2738a53b40
eu10mendonca <eudesjmff@gmail.com>
2026-01-19 01:59:05 -0300

    Adiciona endpoint /hello-world com teste (aula 02)

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,28 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Hello World!"}
+    # return Message(message="Hello World!") também funciona
+
+
+@app.get("/hello-world", status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello_world():
+    return """
+    <html>
+        <head>
+            <title>Hello World</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,11 @@ def test_root_deve_retornar_hello_world():
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello World!"}
+
+
+def test_get_hello_world_deve_retornar_html_com_hello_world():
+    client = TestClient(app)
+    response = client.get("/hello-world")
+    assert response.status_code == HTTPStatus.OK
+    assert "<!DOCTYPE html>" in response.text or "<html>" in response.text
+    assert "<h1>Hello World!</h1>" in response.text
